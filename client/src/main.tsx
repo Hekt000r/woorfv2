7051fa369c956d573c7dc252f71e6baa2ccc4c6d
Hektor <hektorzaimi.dev@gmail.com>
2025-04-24 18:25:56 +0200

    switch back to data routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Navbar from "./Components/Navbar.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([{ path: "/", Component: App }]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Navbar />
+    <RouterProvider router={router}/>
   </StrictMode>
 );
